a0830ffa0d6d8cecb41ee0d44afdb6da0bffe063
Martin Tudor <martcan27@gmail.com>
2019-09-12 15:39:18 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hello");
+const buttons = document.querySelector(".buttons");
+const body = document.querySelector("body");
+
+function clickButton(e) {
+    const {style} = e.target;
+    if (e.target && e.target.nodeName === "BUTTON") {
+        const org = style.background;
+        style.cssText = "background: green; border: 5px solid black";
+        setTimeout(() => (style.cssText = `background: ${org}`), 300);
+    }
+}
+
+buttons.addEventListener("click", clickButton);
