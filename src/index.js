7e539340bac05e028c4b4f7ba62765d6752a92e0
Martin Tudor <martcan27@gmail.com>
2019-09-15 15:16:36 +0100

    styling

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ function clickButton(e) {
     const {style} = e.target;
     if (e.target && e.target.nodeName === "BUTTON") {
         const org = e.target.classList[1];
-        style.cssText = "background: white; border: 5px solid black";
+        style.cssText = "background: white; border: 5px solid " + org;
         setTimeout(() => (style.cssText = `background: ${org}`), 200);
     }
 }
