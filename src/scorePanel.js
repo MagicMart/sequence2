875a40ec1632fa5b8e6e0afc955e6f2cd9d356ae
Martin Tudor <martcan27@gmail.com>
2019-09-16 18:19:51 +0100

    return state

--- src/scorePanel.js
+++ src/scorePanel.js
@@ -11,9 +11,11 @@ const score = window.document.querySelector(".score");
 export function setLives(l: number = 3) {
     state.lives = state.lives + l;
     lives.textContent = state.lives;
+    return state.lives;
 }
 
 export function setScore(s: number = 0) {
     state.score = state.score + s;
     score.textContent = state.score;
+    return state.score;
 }
